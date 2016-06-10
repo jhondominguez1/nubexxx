ec888f689319eadc7c6d3c0096b730d70688205b
BrianUdenar <Brianenriquezcudenar@gmail.com>
2016-06-10 09:42:17 -0500

    Se creo un formulario sencillo para el registro de cursos

---
+++ registrar_curso.php
@@ -0,0 +1,22 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Registro de curso</title>
+</head>
+<body>
+
+<form action="registrar_curso.php" method="POST">
+	
+digite nombre del curso &nbsp;&nbsp;<input type="text" name="nom_curso"><br><br>
+	digite nombre del docente &nbsp;&nbsp;<input type="text" name="nom_docente"><br><br>
+	<input type="submit" value="Registrar curso">
+	</form>
+</body>
+</html>
