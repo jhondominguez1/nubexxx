93cf8970dc3f94dd06b375df33eda046c688c9b7
BrianUdenar <Brianenriquezcudenar@gmail.com>
2016-06-10 09:33:37 -0500

    Se creo la conexion con la base de datos sis_eva1000

---
+++ conexion.php
@@ -0,0 +1,15 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$host="localhost";
+$user="root";
+$password="";
+$bd="sis_eva1000";
+$conexion=mysqli_connect($host,$user,$password,$bd) or die("la conexión no se realizo");
+
+?>
